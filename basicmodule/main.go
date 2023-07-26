9bf875af33e858da9a89a45e9a4b38254bf08013
QuincyForbes <quincyforbes@pm.me>
2023-07-25 23:22:14 -0700

    Fix

--- basicmodule/main.go
+++ basicmodule/main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/tokens/:cid", handlers.GetItemByCID) // Just reference the function, don't call it
-	r.GET("/tokens", handlers.GetAllItems)       // Same here
+	r.GET("/tokens/:cid", handlers.GetItemByCID) 
+	r.GET("/tokens", handlers.GetAllItems)       
 
-	r.Run() // Default runs on :8080
+	r.Run() 
 }
 
